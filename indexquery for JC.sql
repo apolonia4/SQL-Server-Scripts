5a50be035f82fd1850cfc9ded419349eb2ed9df3
fizzmo <Joshua.a.mccoy@gmail.com>
2014-08-08 11:09:28 -0400

    Update indexquery for JC.sql

--- indexquery for JC.sql
+++ indexquery for JC.sql
@@ -1,5 +1,5 @@
 SELECT st.name as TableName, si.name as IndexName, * FROM sys.dm_db_index_physical_stats
-    (DB_ID(N'HinesSolarWindsOrion'), NULL, NULL, NULL , 'LIMITED') IPS
+    (DB_ID(N'dbname'), NULL, NULL, NULL , 'LIMITED') IPS
    JOIN sys.tables ST WITH (nolock) ON IPS.object_id = ST.object_id
    JOIN sys.indexes SI WITH (nolock) ON IPS.object_id = SI.object_id AND IPS.index_id = SI.index_id
 WHERE ST.is_ms_shipped = 0
@@ -12,6 +12,6 @@ select * from sys.tables where object_id = 6291082
 
 
 select * FROM sys.dm_db_index_physical_stats
-    (DB_ID(N'HinesSolarWindsOrion'), NULL, NULL, NULL , 'SAMPLED') --option are LIMITED, DETAILED, SAMPLED 
+    (DB_ID(N'dbname'), NULL, NULL, NULL , 'SAMPLED') --option are LIMITED, DETAILED, SAMPLED 
 	where page_count > 1000
 	order by avg_fragmentation_in_percent desc
